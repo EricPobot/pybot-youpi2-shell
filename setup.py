0b1dbebfc3cf76592340775230aca466c98a369b
Eric Pascual <eric@pobot.org>
2016-08-11 22:51:39 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybot-youpi2-shell',
+    setup_requires=['setuptools_scm'],
+    use_scm_version={
+        'write_to': 'src/pybot/youpi2/__version__.py'
+    },
+    namespace_packages=['pybot.youpi2'],
+    packages=find_packages("src"),
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='Eric Pascual',
+    author_email='eric@pobot.org',
+    install_requires=['pybot-youpi2', 'pybot-lcd-fuse>=0.20.1'],
+    download_url='https://github.com/Pobot/PyBot',
+    description='Youpi2 arm runtime shell',
+    entry_points={
+        'console_scripts': [
+            'youpi2-shell = pybot.youpi2.shell.controller:main',
+        ]
+    }
+)
