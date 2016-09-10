3472c5e859988bd76cd0dfe1e84d926c2361e76a
Eric Pascual <eric@pobot.org>
2016-09-10 23:38:07 +0200

    added implementation of arm controls

--- src/pybot/youpi2/shell/actions/youpi_maint.py
+++ src/pybot/youpi2/shell/actions/youpi_maint.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from pybot.youpi2.model import YoupiArm
+
 from .base import Action
 
 __author__ = 'Eric Pascual'
@@ -9,20 +11,32 @@ _all__ = ['Reset', 'Disable']
 
 class Reset(Action):
     def execute(self):
+        # disable Youpi motors
+        self.arm.hard_hi_Z()
+
         self.panel.clear()
         self.panel.display_splash(
             """Place Youpi in
             home position, then
             press a button.
-        """)
+        """, delay=0)
         self.panel.wait_for_key()
         self.panel.leds_off()
-        self.panel.display_splash("Resetting Youpi...")
+
+        self.panel.display_splash("Seeking origins\n...", delay=0)
+        self.arm.seek_origins(YoupiArm.MOTORS_ALL)
+
+        self.panel.display_splash("Calibrating gripper\n...", delay=0)
+        self.arm.calibrate_gripper(wait=True)
+
+        self.panel.display_splash('Complete.')
 
 
 class Disable(Action):
     def execute(self):
-        # TODO disable Youpi motors
+        # disable Youpi motors
+        self.arm.hard_hi_Z()
+
         self.panel.clear()
         self.panel.display_splash("""
             Youpi motors
